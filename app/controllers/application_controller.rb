a4f8749f857df0320bd344bd766a6c27a845a573
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-12-02 02:36:34 -0600

    Sign in / out completed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
   before_action :initialize_session
   helper_method :cart
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :address, :province_id, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :address, :province_id, :email, :password, :current_password)}
+  end
 
   private
 
