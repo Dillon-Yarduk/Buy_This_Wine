cb62d84e3742afcddcb97527b690e45cfbba215f
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-12-01 20:34:41 -0600

    Added cart and session methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+
+  def initialize_session
+    sesssion[:shopping_cart] ||= []
+  end
+
+  def cart
+    Wine.find(:shopping_cart)
+  end
 end
