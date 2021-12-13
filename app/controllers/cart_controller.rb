728db956ec959e54822f7bd33dc7cef0b68793f2
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-12-13 15:48:19 -0600

    Just checking.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,29 +2,22 @@ class CartController < ApplicationController
   def create
     wine_id = params[:id].to_i
     quantity = params[:quantity].to_i
-    wine = Wine.find(wine_id)
 
-    wine_order = WineOrder.new(
-      quantity: quantity,
-      price:    quantity * wine.price,
-      wine:     wine
-    )
-
-    session[:shopping_cart] << wine_order
+    session[:shopping_cart] << {wine_id: wine_id, quantity: quantity}
     redirect_to root_path
   end
 
   def update
     wine_id = params[:id].to_i
     quantity = params[:quantity].to_i
-    wine_order = session[:shopping_cart].detect {|wine_order| wine_order["wine_id"] == wine_id}
+    wine_order = session[:shopping_cart].detect {|x| x["wine_id"] == wine_id}
     wine_order["quantity"] = quantity
     redirect_to root_path
   end
 
   def destroy
     wine_id = params[:id].to_i
-    wine_order = session[:shopping_cart].detect {|wine_order| wine_order["wine_id"] == wine_id}
+    wine_order = session[:shopping_cart].detect {|x| x["wine_id"] == wine_id}
     session[:shopping_cart].delete(wine_order)
     redirect_to root_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
   resources :varieties, only: %i[index show]
   resources :regions, only: %i[index show]
   resources :cart, only: %i[create update destroy]
+  # scope "/checkout" do
+  #   post "create" to: "checkout#create", as: "checkout_create"
+  #   get "success" to: "checkout#success", as: "checkout_success"
+  #   get "cancel" to: "checkout#cancel", as: "checkout_cancel"
+  # end
   root to: "wines#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
