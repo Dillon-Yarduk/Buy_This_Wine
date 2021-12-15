ee4d9bdee33a66db2a52d60f8f8891c770d649dc
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-12-14 20:40:02 -0600

    Flash notice added to cart actions.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,6 +4,10 @@ class CartController < ApplicationController
     quantity = params[:quantity].to_i
 
     session[:shopping_cart] << { wine_id: wine_id, quantity: quantity }
+    wine = Wine.find(wine_id)
+    flash[:notice] =
+      "#{quantity} #{quantity > 1 ? 'bottles' : 'bottle'} of
+      #{wine.name}, #{wine.variety.name} - added to cart."
     redirect_to root_path
   end
 
@@ -11,7 +15,12 @@ class CartController < ApplicationController
     wine_id = params[:id].to_i
     quantity = params[:quantity].to_i
     wine_order = session[:shopping_cart].detect { |x| x["wine_id"] == wine_id }
+    old_quantity = wine_order["quantity"]
     wine_order["quantity"] = quantity
+    wine = Wine.find(wine_id)
+    flash[:notice] =
+      "#{wine.name}, #{wine.variety.name} - order changed from #{old_quantity}
+      to #{quantity} #{quantity > 1 ? 'bottles' : 'bottle'}"
     redirect_to root_path
   end
 
@@ -19,6 +28,8 @@ class CartController < ApplicationController
     wine_id = params[:id].to_i
     wine_order = session[:shopping_cart].detect { |x| x["wine_id"] == wine_id }
     session[:shopping_cart].delete(wine_order)
+    wine = Wine.find(wine_id)
+    flash[:notice] = "#{wine.name}, #{wine.variety.name} - removed from cart."
     redirect_to root_path
   end
 
