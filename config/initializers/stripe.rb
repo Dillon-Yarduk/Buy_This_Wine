d55f97d8dd204af74506c91d8f43bb4ced8f7122
Dillon-Yarduk <dillonyarduk@gmail.com>
2021-12-16 13:37:35 -0600

    another commit

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV["PUBLISHABLE_KEY"],
-  secret_key:      ENV["SECRET_KEY"]
+  publishable_key: "pk_test_51JtZKHKd52UTKY0TOA5NnTWMoZwvCe1hosIFG4gli6cl71jRXgCBg1rGfeLZuQKTdb5M5fTgsYg2MlkFo0oxrj3p00GIk2C0HX",
+  secret_key:      "sk_test_51JtZKHKd52UTKY0TxeMn7tqlbC5uPrsfdyttsttraKeP74HlWoypgfXXskE0dW6O7c1dxD7EIyvrJUQdxzyGWzWb007vSyrHQ4"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
